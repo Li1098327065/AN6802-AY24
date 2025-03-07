6fb05e13a780d6885e52f115c3fe177dd95cc396
Li1098327065 <YLI157@e.ntu.edu.sg>
2025-03-07 13:16:46 +0000

    1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/",methods=["POST","GET"])
 def index():
@@ -8,13 +8,20 @@ def index():
 
 @app.route("/main",methods=["POST","GET"])
 def main():
-    user_name=request.form.get("q")
+    user_name = request.form.get("q")
     return(render_template("main.html"))
 
 @app.route("/ethical_test",methods=["POST","GET"])
 def ethical_test():
     return(render_template("ethical_test.html"))
 
+@app.route("/test_result",methods=["POST","GET"])
+def test_result():
+    answer = request.form.get("answer")
+    if answer=="false":
+        return(render_template("pass.html"))
+    elif answer=="true":
+        return(render_template("fail.html"))
 
 if __name__ == "__main__":
     app.run()
